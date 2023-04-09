6761db2bdfbc87138fdc6d66b287402e22572ba7
engineer7670 <130302272+engineer7670@users.noreply.github.com>
2023-04-09 21:43:53 +0530

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,37 @@
+{
+  "package": {
+    "packagename":"com.downloader.videodownloader.hd.allstatussaver",
+    "vpn show":"true",
+    "vpn_id":"touchvpn",
+    "vpn_host":"test.test.com",
+    "vpn_country":"CH,CA,GB,US,UK,DE",
+    "countrynamelist":"Australia,Brazil,Canada,China,Colombia,France,Germany,Indonesia,Ireland,Israel,Italy,Japan,Malaysia,Netherlands,New Zealand,North Korea,Philippines,Poland,Singapore,Slovakia,South Korea,Spain,Switzerland,Thailand,Ukraine,United Arab Emirates,United Kingdom,United States,Vietnam,Hong Kong",
+    "vpn_ads_click_inter":"2",
+    "vpn_is_native_banner":"false",
+    "is_ads_click_inter":"2 ",
+    "is_native_banner":"true",
+    "is_ads":"true",
+    "privacypolicy":"https://google.com/privacypolicy",
+    "qureka_url":"https://google.com/",
+    "admob_interstitial_id_list": [
+      "ca-app-pub-3940256099942544/1033173712",
+      "ca-app-pub-3940256099942544/1033173712"
+    ],
+    "admob_native_id_list": [
+      "ca-app-pub-3940256099942544/2247696110",
+      "ca-app-pub-3940256099942544/2247696110"
+    ],
+    "admob_bottom_native_id_list": [
+      "ca-app-pub-3940256099942544/2247696110",
+      "ca-app-pub-3940256099942544/2247696110"
+    ],
+    "admob_banner_id_list": [
+      "ca-app-pub-3940256099942544/6300978111",
+      "ca-app-pub-3940256099942544/6300978111"
+    ],
+    "app_open_id_list": [
+      "ca-app-pub-3940256099942544/3419835294",
+      "ca-app-pub-3940256099942544/3419835294"
+    ]
+  }
+}
