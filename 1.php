b226fc2c2df9ccf3fb17e0fb11d7a56d18cc74be
engineer7670 <130302272+engineer7670@users.noreply.github.com>
2023-04-10 11:41:41 +0530

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,18 +1,6 @@
 {
   "package": {
     "packagename":"com.downloader.videodownloader.hd.allstatussaver",
-    "vpn show":"true",
-    "vpn_id":"touchvpn",
-    "vpn_host":"test.test.com",
-    "vpn_country":"CH,CA,GB,US,UK,DE",
-    "countrynamelist":"Australia,Brazil,Canada,China,Colombia,France,Germany,Indonesia,Ireland,Israel,Italy,Japan,Malaysia,Netherlands,New Zealand,North Korea,Philippines,Poland,Singapore,Slovakia,South Korea,Spain,Switzerland,Thailand,Ukraine,United Arab Emirates,United Kingdom,United States,Vietnam,Hong Kong",
-    "vpn_ads_click_inter":"2",
-    "vpn_is_native_banner":"false",
-    "is_ads_click_inter":"2 ",
-    "is_native_banner":"true",
-    "is_ads":"true",
-    "privacypolicy":"https://google.com/privacypolicy",
-    "qureka_url":"https://google.com/",
     "vpn_master": "1",
     "vpn_enable": null,
     "vpn_host_id": "https://d2isj403unfbyl.cloudfront.net",
@@ -20,6 +8,7 @@
     "country_list": "us",
     "country_ignore": null,
     "show_banner_enable": "1",
+    "is_ads":"true",
     "admob_interstitial_id_list": [
       "ca-app-pub-3940256099942544/1033173712",
       "ca-app-pub-3940256099942544/1033173712"
